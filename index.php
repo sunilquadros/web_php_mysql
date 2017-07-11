bb21b3fa150f7ae4952758237114cded77c49613
sunilquadros <sunilquadros@workstation.lab.example.com>
2017-07-11 06:07:45 -0400

    2nd commit

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <html>
-<head></head>
-<?php
-require('./file.php');
-?>
+<head>
+<style>
+label{display:inline-block;width:100px;margin-bottom:10px;}
+</style>
+ 
+ 
+<title>Add Employee</title>
+</head>
+<body>
+ 
+<form method="post" action="">
+<label>First Name</label>
+<input type="text" name="first_name" />
+<br />
+<label>Last Name</label>
+<input type="text" name="last_name" />
+<br />
+<label>Department</label>
+<input type="text" name="department" />
+<br />
+<label>Email</label>
+<input type="text" name="email" />
+ 
+<br />
+<input type="submit" value="Add Employee">
+</form>
+ 
+ 
+ 
+</body>
 </html>
