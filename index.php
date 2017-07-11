007503eadf3b1962b9c4ed6cb778a5aebc479757
root <root@workstation.lab.example.com>
2017-07-11 12:00:50 -0400

    sixth commit

--- index.php
+++ index.php
@@ -1,44 +1,33 @@
+<!DOCTYPE html>
 <html>
 <head>
-<!-- <?php
-require('./file.php');
-?> -->
 <style>
 label{display:inline-block;width:100px;margin-bottom:10px;}
 </style>
- 
- 
-<title>Add Record</title>
+
+
+<title>Add Employee</title>
 </head>
 <body>
- 
+
 <form method="post" action="">
-<label>Instructor Number</label>
-<input type="text" name="instructorNumber" />
+<label>First Name</label>
+<input type="text" name="first_name" />
 <br />
-<label>Instructor Name</label>
-<input type="text" name="instructorName" />
+<label>Last Name</label>
+<input type="text" name="last_name" />
+<br />
+<label>Department</label>
+<input type="text" name="department" />
 <br />
 <label>Email</label>
 <input type="text" name="email" />
+
 <br />
-<label>City</label>
-<input type="text" name="city" />
-<br />
-<label>State</label>
-<input type="text" name="state" />
-<br />
-<label>Postal Code</label>
-<input type="text" name="postalCode" />
-<br />
-<label>Country</label>
-<input type="text" name="country" />
- 
-<br />
-<input type="submit" value="Add Record">
+<input type="submit" value="Add Employee">
 </form>
- 
- 
- 
+
+
+
 </body>
 </html>
