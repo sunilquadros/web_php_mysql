e96e9984813bb6023b5135c071696886a437592a
sunilquadros <sunilquadros@hotmail.com>
2017-07-11 12:52:37 -0500

    Adding php mappings

--- index.php
+++ index.php
@@ -6,34 +6,35 @@ label{display:inline-block;width:100px;margin-bottom:10px;}
 </style>
 
 
-<title>Add Employee</title>
+<title>Add Record</title>
 </head>
+  
 <body>
 
-<form method="post" action="">
+<form method="post" action="file.php">
 <label>Instructor Number</label>
-<input type="text" name="instructorNumber" />
+<input type="text" name="instructorNumber" value="instructorNumber" />
 <br />
 <label>Instructor Name</label>
-<input type="text" name="instructorName" />
+<input type="text" name="instructorName" value="instructorName" />
 <br />
 <label>Email</label>
-<input type="text" name="email" />
+<input type="text" name="email" value="email" />
 <br />
 <label>City</label>
-<input type="text" name="city" />
+<input type="text" name="city" value="city" />
 <br />
 <label>State</label>
-<input type="text" name="state" />
+<input type="text" name="state" value="state" />
 <br />
 <label>Postal Code</label>
-<input type="text" name="postalCode" />
+<input type="text" name="postalCode" value="postalCode" />
 <br />
 <label>Country</label>
-<input type="text" name="country" />
+<input type="text" name="country" value="country" />
 
 <br />
-<input type="submit" value="Add Employee">
+<input type="submit" value="Add Record">
 </form>
 
 
