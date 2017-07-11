37ca658247ab4001852b88cb532f6857c8f0d082
sunilquadros <sunilquadros@hotmail.com>
2017-07-11 13:30:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,25 +13,25 @@ label{display:inline-block;width:100px;margin-bottom:10px;}
 
 <form method="post" action="file.php">
 <label>Instructor Number</label>
-<input type="text" name="instructorNumber" value="instructorNumber" />
+<input type="text" name="instructorNumber"  />
 <br />
 <label>Instructor Name</label>
-<input type="text" name="instructorName" value="instructorName" />
+<input type="text" name="instructorName"  />
 <br />
 <label>Email</label>
-<input type="text" name="email" value="email" />
+<input type="text" name="email" />
 <br />
 <label>City</label>
-<input type="text" name="city" value="city" />
+<input type="text" name="city" />
 <br />
 <label>State</label>
-<input type="text" name="state" value="state" />
+<input type="text" name="state"  />
 <br />
 <label>Postal Code</label>
-<input type="text" name="postalCode" value="postalCode" />
+<input type="text" name="postalCode" />
 <br />
 <label>Country</label>
-<input type="text" name="country" value="country" />
+<input type="text" name="country" />
 
 <br />
 <input type="submit" value="Add Record">
